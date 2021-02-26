0775556913155dafd880dba554110e5362dd0b18
mhkmcp <mhkmcp@yahoo.com>
2021-02-27 02:14:23 +0600

    02: Added Insert and Delete

--- index.js
+++ index.js
@@ -3,10 +3,41 @@ var Url = "mongodb+srv://admin123:uBEzp2e2ZViZCLVO@cluster0.jrjv8.mongodb.net?re
 
 var config = { useUnifiedTopology: true };
 
-MongoClient.connect(Url, config, function (error) {
+MongoClient.connect(Url, config, function (error, MyMongoClient) {
     if (error) {
         console.log("Failed!");
     } else {
         console.log("Success");
+        // InsertData(MyMongoClient);
+        DeleteOneItem(MyMongoClient);
     }
-})
+});
+
+function InsertData(MyMongoClient) {
+    var MyDatabase = MyMongoClient.db("node-mongodb");
+    var MyCollection = MyDatabase.collection('table_name');
+    var MyData = { name: "Kabir", roll: "07", city: "Sylhet" };
+
+    MyCollection.insertOne(MyData, function (error) {
+        if (error) {
+            console.log("Insert Failed!");
+        } else {
+            console.log("Insert Success");
+        }
+    })
+}
+
+function DeleteOneItem(MyMongoClient) {
+    var MyDatabase = MyMongoClient.db("node-mongodb");
+    var MyCollection = MyDatabase.collection('table_name');
+    var DeleteItem = { roll: "07" };
+
+    MyCollection.deleteOne(DeleteItem, function (error) {
+        if (error) {
+            console.log("Delete Failed!");
+        } else {
+            console.log("Delete Success");
+        }
+
+    })
+}
